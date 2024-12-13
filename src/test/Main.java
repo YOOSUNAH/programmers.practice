f2446b1b1d7bce4f69b059b7cd23fd3ef42b7683
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-12-13 17:31:23 +0900

    --

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,20 +1,7 @@
-package test;
-
-public class Main {
-
-
-    public void sadfasdf (){
-        try {
-            throw new SunahChenjaeException();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
-
-    public void sadfasdf2 () throws SunahChenjaeException{
-        throw new SunahChenjaeException();
-    }
-
-
-}
+//package test;
+//
+//public class Main {
+//
+//
+//
+//}
